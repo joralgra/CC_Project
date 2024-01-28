768f8525de11ec49008d4516fbf9f7554f39663a
fsalazar5 <fsalazarr@ripley.com>
2024-01-28 17:30:46 +0100

    migration to jetstream api

---
+++ Subsystems/Frontend/src/services/query-service/actions/get-job-list.ts
@@ -0,0 +1,23 @@
+import { natsWrapper } from '../../../config/nats-wrapper';
+
+const jobListByUser = async (user: string) => {
+  const nc = natsWrapper.client;
+  const js = nc.jetstream();
+  const kv = await js.views.kv('jobs');
+  let jobs = [];
+  const iter = await kv.history({ key: `${user}.*` });
+
+  for await (const e of iter) {
+    const job: any = e.json();
+    jobs.push({
+      user: job.user,
+      jobId: job.jobId,
+      state: job.state,
+      timeStamp: job.timeStamp,
+    });
+  }
+
+  return jobs;
+};
+
+export default jobListByUser;
